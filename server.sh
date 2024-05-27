6ec4777ae90c568f484aa2de971296e2175794c5
FrownwithanF <93647766+FrownwithanF@users.noreply.github.com>
2024-05-27 23:46:02 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt install -y default-jdk && wget https://getbukkit.org/latest-dev/craftbukkit-1.5.2-R1.0.jar && java -Xms512M -Xmx1024M -jar craftbukkit-1.5.2-R1.0.jar
+sudo apt update && sudo apt install -y default-jdk && wget https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.5.2-R1.0.jar && java -Xms512M -Xmx1024M -jar craftbukkit-1.5.2-R1.0.jar
